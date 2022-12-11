a08c48c24740360a6d3828d69e63b18f817d08fb
Nick Scofield <c.nick.scofield@gmail.com>
2022-12-11 12:09:29 -0700

    commit tree cf439225e3e8c37876dde45b086abe216ed7df32

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance = 0){
+  
+  if (distance < 401) {
+    return 'This one is on me!';
+  } else if (distance < 2001) {
+    return 'That will be twenty bucks.';
+  } else if (distance < 2501) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city = ""){
+  return (city === "NYC") ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+
 }
